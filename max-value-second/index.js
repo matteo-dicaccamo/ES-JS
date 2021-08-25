8fba592a75cf08acb89c451b01c8fb315d65970a
Matteo Di Caccamo <teo.dicaccamo@gmail.com>
2021-08-25 10:36:07 +0200

    :feat: update max-value-second folder

--- max-value-second/index.js
+++ max-value-second/index.js
@@ -2,11 +2,11 @@
 // parametro un array di numeri e restituisca 
 // il secondo valore più grande presente nell'array         
 
-const arr = [1, 2, 5, 15, 25];
+const array = [1, 2, 5, 15, 25];
 
 function getSecondValueBigger(arr) {
     arr.sort((a, b) => b - a);
     console.log(arr[1]);
 }
 
-getSecondValueBigger(arr);
+getSecondValueBigger(array);
