4b49d7552cc6b3a48480000b7ba8b1cc81c29f18
Matteo Di Caccamo <teo.dicaccamo@gmail.com>
2021-08-25 10:17:47 +0200

    feat: update bomba-timer folder

--- bomba-timer/index.js
+++ bomba-timer/index.js
@@ -10,16 +10,13 @@
 let seconds = Math.floor(Math.random() * (10 - 1) + 1);
 
 const timer = () => {
-
     console.log(`Mancano ${seconds} secondi all'esplosione della bomba`);
 
     if (seconds === 0) {
         console.log("BOOM!");
         clearInterval(id);
     }
-
     --seconds;
-
 };
 
 const id = setInterval(() => timer(), 1000);
